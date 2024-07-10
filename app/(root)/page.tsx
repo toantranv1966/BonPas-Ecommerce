ae3d7e67404ec22a1d13ff9cfd1638c94de3bd9d
Toan Tran <toan.tranv1966@gmail.com>
2024-07-10 13:26:44 +0700

    07. load products from database

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,12 @@
 import ProductList from '@/components/shared/product/product-list'
-import sampleData from '@/lib/sample-data'
+import { getLatestProducts } from '@/lib/actions/product.actions'
 
-export default function Home() {
+export default async function Home() {
+  const lastestProducts = await getLatestProducts()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="space-y-8">
-        <h2 className="h2-bold">Latest Products</h2>
-        <ProductList data={sampleData.products} />
+        <ProductList title="Newest Arrivals" data={lastestProducts} />
       </div>
     </main>
   )
